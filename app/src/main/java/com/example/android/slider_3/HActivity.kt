dd46d30170f666d85d4a5c8483a6c574800e470f
Ibrahimadhraf <33137985+Ibrahimadhraf@users.noreply.github.com>
2019-05-24 03:36:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/slider_3/HActivity.kt
@@ -0,0 +1,19 @@
+package com.example.android.slider_3
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class HActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_h)
+
+    }
+
+}
